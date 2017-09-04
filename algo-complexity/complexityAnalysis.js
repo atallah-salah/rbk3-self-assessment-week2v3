755e199a303689bb211f8336a81989473905a977
atallah-salah <atallah_salah@outlook.com>
2017-09-04 09:42:36 +0300

    hehe!

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -2,5 +2,11 @@
 // Be as clear and complete as necessary to demonstrate your mastery.
 // Write your explanation below as a comment
 /*
+Liknedlist
 
+o(n) : that mean it need n time to finsh the function(job) in n line
+o(1) : that mean it done the fuction only one time in one line, like only one for loop
+o(n*2):that mean it will take double time n
+
+i didn't understand it i try but i found it hard (algorithmic time complexity)
 */
